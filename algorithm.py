13ea8683d61b22bfdc7f4e206199b1c05083ac5f
llihcchill <125551072+llihcchill@users.noreply.github.com>
2023-03-07 17:23:23 +1000

    Create algorithm.py

---
+++ algorithm.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+import masteroogway
+import nltk
+
+# advice quotes api to feed into the machine learning algorithm
+
+
+# process question language to feed it into machine learning algorithm
+
+
+# get question input
+    # make question input readable (put all letters to lowercase etc.)
+
+
+# use dictionary to get only the nouns, verbs, and adjectives (ready to feed into algorithm)
+
+
+# model that will be trained on advice quotes api
+class masteroogwayAdviceModel(tf.keras.Model):
+    def __init__(self) -> None:
+        super(masteroogwayAdviceModel).__init__()
+        # model parameters
