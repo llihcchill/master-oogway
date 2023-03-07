ec7fceb30e54e5d5cecc86d20a72f4abdad16ca4
llihcchill <125551072+llihcchill@users.noreply.github.com>
2023-03-07 17:20:58 +1000

    Update algorithm.py
     - Removed Tensorflow folder as it was unnecessary
     - Removed importlib to replace it will normal import (works to the end it needs to)
     - Imported NTLK library to to natural language processing

--- tensorflow/algorithm.py
+++ tensorflow/algorithm.py
@@ -1,15 +1,11 @@
 import tensorflow as tf
-import numpy
-import importlib
-
-# import the Tensorflow algorithm into the main script
-masteroogway = input('masteroogway')
-importlib.import_module(masteroogway)
+import masteroogway
+import nltk
 
 # advice quotes api to feed into the machine learning algorithm
 
 
-# dictionary of every noun, verb, and adjective to comprehend questions (maybe another api?)
+# process language
 
 
 # get question input
