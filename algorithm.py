32b04678fef083c01eb9237769dd2abe1c5e38d6
llihcchill <125551072+llihcchill@users.noreply.github.com>
2023-03-07 21:55:54 +1000

    Update algorithm.py
     - Pre-processed the question for putting into the model

--- algorithm.py
+++ algorithm.py
@@ -1,15 +1,37 @@
 import tensorflow as tf
+from tensorflow.python.keras.layers import Embedding, Dense, LSTM
+from tensorflow.python.keras.models import Sequencial
+from tensorflow.python.keras.losses import MeanSquaredError
 import masteroogway
 import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
 
 # advice quotes api to feed into the machine learning algorithm
 
 
-# process question language to feed it into machine learning algorithm
+# process language to put into ml algorithm
 
 
 # get question input
     # make question input readable (put all letters to lowercase etc.)
+
+# tokenise question into its words
+main_question = masteroogway.question
+text = f"""{main_question}"""
+tokenised_text = word_tokenize(text)
+
+# filter the question into its key words
+filtered_sentence = []
+stop_words = set(stopwords.words("english"))
+for words in stop_words:
+    if words not in stop_words:
+        filtered_sentence.append(words)
+
+# stem and lemmonise the words to get them prepared for model building
+
+
+
 
 
 # use dictionary to get only the nouns, verbs, and adjectives (ready to feed into algorithm)
