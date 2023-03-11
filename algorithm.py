e2fcaad1405db7069202d2ea4f8d0c104884fd9c
llihcchill <125551072+llihcchill@users.noreply.github.com>
2023-03-11 20:02:21 +1000

    Update algorithm.py
     - Added stemming of question

--- algorithm.py
+++ algorithm.py
@@ -1,23 +1,20 @@
+import masteroogway
 import tensorflow as tf
 from tensorflow.python.keras.layers import Embedding, Dense, LSTM
 from tensorflow.python.keras.models import Sequencial
 from tensorflow.python.keras.losses import MeanSquaredError
-import masteroogway
 import nltk
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
 
 # advice quotes api to feed into the machine learning algorithm
 
 
-# process language to put into ml algorithm
-
-
 # get question input
-    # make question input readable (put all letters to lowercase etc.)
+main_question = masteroogway.question
 
 # tokenise question into its words
-main_question = masteroogway.question
 text = f"""{main_question}"""
 tokenised_text = word_tokenize(text)
 
@@ -29,7 +26,10 @@ for words in stop_words:
         filtered_sentence.append(words)
 
 # stem and lemmonise the words to get them prepared for model building
-
+ps = PorterStemmer()
+stemmed_words = []
+for word in filtered_sentence:
+    stemmed_words.append(ps.stem(word))
 
 # use dictionary to get only the nouns, verbs, and adjectives (ready to feed into algorithm)
 
