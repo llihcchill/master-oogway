4ed461ebfa8966735315d0a7600330b9dbe635a0
llihcchill <125551072+llihcchill@users.noreply.github.com>
2023-03-18 19:05:17 +1000

    Update algorithm.py
     - Added quote API functionality
     - Put lemmonising, stemming, and filtering text into functions

--- algorithm.py
+++ algorithm.py
@@ -14,28 +14,54 @@ import requests
 text = f"""{question}"""
 tok = tokenised_text = word_tokenize(text)
 
+
 # filter the question into its key words
-filtered_sentence = []
-stop_words = set(stopwords.words("english"))
-for words in stop_words:
-    if words not in stop_words:
-        filtered_sentence.append(words)
+def filter_sentence(filtered_sentence_array):
+    stop_words = set(stopwords.words("english"))
+    for words in stop_words:
+        if words not in stop_words:
+            filtered_sentence_array.append(words)
 
 # stem and lemmonise the words to get them prepared for model building
-ps = PorterStemmer()
-stemmed_words = []
-for word in filtered_sentence:
-    stemmed_words.append(ps.stem(word))
+def stem_sentence(filtered_sentence_array, stemmed_words_array):
+    ps = PorterStemmer()
+    for word in filtered_sentence_array:
+        stemmed_words_array.append(ps.stem(word))
 
 # lemmonise stemmed words
-lem = WordNetLemmatizer()
-lemmonised_words = []
-for word in stemmed_words:
-    lemmonised_words.append(lem.lemmatize(word))
+def lem_sentence(stemmed_words_array, lemmonised_words_array):
+    lem = WordNetLemmatizer()
+    for word in stemmed_words_array:
+        lemmonised_words_array.append(lem.lemmatize(word))
+
+# define the arrays for filtering, stemming, and lemmonising the question
+filtered_question = [], stemmed_question = [], lemmonised_question = []
+
+# run each function for each stage of the word preperation
+filter_sentence(filtered_question)
+stem_sentence(filtered_question, stemmed_question)
+lem_sentence(stemmed_question, lemmonised_question)
 
 # advice quotes api to feed into the machine learning algorithm
 # the api that will be used is the ZenQuotes api here: https://zenquotes.io/
 api_call = requests.get("https://zenquotes.io/api/quotes")
+api_quotes = api_call.json()["api_call"]
+
+# define the arrays for filtering, stemming, and lemmonising the quote
+filtered_quote = [], stemmed_quote = [], lemmonised_quote = [], final_quote = []
+
+# append every quote to single_api_quotes
+for quote in api_quotes:
+    quotes = quote["q"]
+    # run functions to prepare quote
+    filter_sentence(filtered_quote)
+    stem_sentence(filtered_quote, stemmed_quote)
+    lem_sentence(stemmed_quote, lemmonised_quote)
+    if(lemmonised_quote.__contains__(lemmonised_question) == True):
+        final_quote.append(lemmonised_quote)
+    else:
+        break
+
 
 # model that will be trained on advice quotes api
 class masteroogwayAdviceModel(tf.keras.Model):
