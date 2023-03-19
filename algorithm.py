90b6dd6f14b78fde41987a2aa9700cf36a6ace7d
llihcchill <125551072+llihcchill@users.noreply.github.com>
2023-03-20 06:49:44 +1000

    Update algorithm.py
     - Put the three filter_sentence, stem_sentence, and lemmonise_sentence functions into one function

--- algorithm.py
+++ algorithm.py
@@ -14,22 +14,19 @@ import requests
 text = f"""{question}"""
 tok = tokenised_text = word_tokenize(text)
 
-
-# filter the question into its key words
-def filter_sentence(filtered_sentence_array):
+def prepare_sentence(filtered_sentence_array, stemmed_words_array, lemmonised_words_array):
+    # filter the question into its key words
     stop_words = set(stopwords.words("english"))
     for words in stop_words:
         if words not in stop_words:
             filtered_sentence_array.append(words)
 
-# stem and lemmonise the words to get them prepared for model building
-def stem_sentence(filtered_sentence_array, stemmed_words_array):
+    # stem the words to cut any prefixes
     ps = PorterStemmer()
     for word in filtered_sentence_array:
         stemmed_words_array.append(ps.stem(word))
 
-# lemmonise stemmed words
-def lem_sentence(stemmed_words_array, lemmonised_words_array):
+    # lemmonise the stemmed words
     lem = WordNetLemmatizer()
     for word in stemmed_words_array:
         lemmonised_words_array.append(lem.lemmatize(word))
@@ -37,10 +34,8 @@ def lem_sentence(stemmed_words_array, lemmonised_words_array):
 # define the arrays for filtering, stemming, and lemmonising the question
 filtered_question = [], stemmed_question = [], lemmonised_question = []
 
-# run each function for each stage of the word preperation
-filter_sentence(filtered_question)
-stem_sentence(filtered_question, stemmed_question)
-lem_sentence(stemmed_question, lemmonised_question)
+# run the function to prepare the sentence for model building
+prepare_sentence(filtered_question, stemmed_question, lemmonised_question)
 
 # advice quotes api to feed into the machine learning algorithm
 # the api that will be used is the ZenQuotes api here: https://zenquotes.io/
@@ -50,13 +45,11 @@ api_quotes = api_call.json()["api_call"]
 # define the arrays for filtering, stemming, and lemmonising the quote
 filtered_quote = [], stemmed_quote = [], lemmonised_quote = [], final_quote = []
 
-# append every quote to single_api_quotes
+# append every quote (if it matches a word in the lemmonised question) to final_quote
 for quote in api_quotes:
     quotes = quote["q"]
-    # run functions to prepare quote
-    filter_sentence(filtered_quote)
-    stem_sentence(filtered_quote, stemmed_quote)
-    lem_sentence(stemmed_quote, lemmonised_quote)
+    # run function to prepare quote
+    prepare_sentence(filtered_quote, stemmed_quote, lemmonised_quote)
     if(lemmonised_quote.__contains__(lemmonised_question) == True):
         final_quote.append(lemmonised_quote)
     else:
