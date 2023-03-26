c704d2c0bd63172accab91834cc8504cb12bb1f2
llihcchill <125551072+llihcchill@users.noreply.github.com>
2023-03-26 14:43:52 +1000

    Update masteroogway.py
     - Sends message of the best quote found in the algorithm.py file

--- src/masteroogway.py
+++ src/masteroogway.py
@@ -4,7 +4,6 @@ from algorithm import run_algorithm
 
 # add discord intents and instantiate the discord client
 intents = discord.Intents.default()
-intents = True
 client = discord.Client(intents = intents)
 
 # if errors occur, this function will print them out into a file called "masteroogwayerr.log"
@@ -17,16 +16,21 @@ async def on_ready(self):
 
 @client.event
 async def on_message(self, message):
+    # tests to see if the message sent is from itself
     if message.author == client.user:
         return
+    else:
+        print(f"Logged in as {self.user}")
 
     if message.content.startsWith("?"):
         # initialise question and array to store the quote
-        question = message.content
-        quote = []
+        question = message.content, quote = []
+
+        # run run_algorithm function to find perfect quote
         run_algorithm(question, quote)
 
         # send a message for the quote in italic (make it look good as well)
+        await message.channel.send(str(quote[0]))
 
 # runs the bot
 client.run("token here", log_handler = handler, log_level = logging.DEBUG)
